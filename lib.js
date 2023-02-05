92ed12d2bba585085afc5aee0291c2617b864a96
uebpiick <60529857+uebpiick@users.noreply.github.com>
2023-02-05 18:48:03 +0900

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,13 +1,13 @@
-exports.btoaURL = (string) => {
+export function btoaURL(string) {
 	return btoa(unescape(encodeURIComponent(string)))
 		.replace(/\+/g, "-")
 		.replace(/\//g, "_")
 		.replace(/=/g, "");
-};
-exports.atobURL = (string) => {
+}
+export function atobURL(string) {
 	string = string.replace(/-/g, "+").replace(/_/g, "/");
 	while (string.length % 4) {
 		string += "=";
 	}
 	return decodeURIComponent(escape(atob(string)));
-};
+}
